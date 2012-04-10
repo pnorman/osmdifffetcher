e4e25a10567becdb47d70312355805dff3a28c6f
Paul Norman <penorman@mac.com>
2012-04-10 14:07:24 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,25 @@
+'''
+	This file is part of osmdifffetcher.
+
+	osmdifffetcher is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	osmdifffetcher is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with osmdifffetcher.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright 2012 Paul Norman
+'''
+
+import sys
+import urllib2
+from datetime import datetime
+import time
+import os
+import errno
